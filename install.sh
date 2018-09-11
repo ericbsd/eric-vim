1ddd1e687ae03880b1d23d72b89f36c2f3e19452
ericbsd <ericturgeon.bsd@gmail.com>
2018-09-11 07:04:12 -0300

    new setup for install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,14 @@
 echo "Copying vimrc file"
 cp vimrc ~/.vimrc
 echo "Copying vim folder"
-cp -R vim ~/.vim
+mkdir -p ~/.vim/bundle
 echo "Installing packages dependency"
-sudo pkg install vim git boost-all py27-boost-libs py36-powerline-status powerline-fonts
+sudo pkg install vim git boost-all py36-boost-libs py36-powerline-status powerline-fonts
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim -c 'PluginInstall'
+
+cd ~/.vim/bundle/YouCompleteMe/
+python3.6 install.py
+cd -
 
